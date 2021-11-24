a87f805507b1c0f04fc65ec9fba75dd4da1043c4
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2021-11-24 22:32:02 +0300

    ground

--- vis.py
+++ vis.py
@@ -1,1 +1,51 @@
-import math
+import sys
+import pyglet
+import pygame
+from pyglet.gl import *
+from noise import pnoise1
+
+
+RED = 0xFF0000
+BLUE = 0x0000FF
+YELLOW = 0xFFC91F
+GREEN = 0x00FF00
+MAGENTA = 0xFF03B8
+CYAN = 0x00FFCC
+BLACK = (0, 0, 0)
+WHITE = 0xFFFFFF
+GREY = 0x7D7D7D
+
+WIDTH = 800
+HEIGHT = 600
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+span = 5
+points = 256
+base = 0
+octaves = 1
+
+r = range(256)
+a = -1000
+b = 300
+
+for i in r:
+    x = (float(i) * span / points - 0.5 * span)*400
+    y = pnoise1(x + base, octaves)*100+400
+    pygame.draw.line(screen, WHITE, [a, b], [x, y], 3)
+    a = x
+    b = y
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+FPS = 30
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
